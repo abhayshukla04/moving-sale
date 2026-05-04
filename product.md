e9012413e1bec5d690a83a52133b77bd9480de59
Abhay Shukla <abhayshukla@3e:75:c0:b6:9b:fd.home>
2026-05-03 20:27:55 -0400

    items

--- product.md
+++ product.md
@@ -1,163 +1,42 @@
-## Air conditoner
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Matteres : IKEA
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Matteres : DUROFLEX
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## IKEA bed iron court
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## portable tea table 2
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Study Table for boy
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Stydy table for girl
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Study chair for boy
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Stydy chair for girl
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Wardrob
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Wardrob
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Bike : age 9-10
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Bike : age 4-5
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Samsung TV 55 inch
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Recliner Dufoflex single seater : count 2
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## IKEA light
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## IRON Stand
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## IRON
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Chrismas tree
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Sofa 3 seater
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## IKEA Steel rack
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## TV rack with storage
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## IKEA TABLE : count 2
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Office table : 1
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## blanket : king size 2
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Mixer prreti with juicer
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Air fryer with Oven
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## cashroll : milton
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Superhero toys
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Soft toys for kids
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Steel Plate
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Steel Glass
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## King size bed protector
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Dining chair 4
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Robot vaccume cleaner
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Humidifier
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Water filter
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Stemer
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Bedsheet king size
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## Kitchen Utencils made out of steel
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)
-
-## weight machine
-![Image 1](img/photo-1494526585095-c41746248156.avif)
-![Image 2](img/photo-1494526585095-c41746248156.avif)  
+1. Air conditoner 
+2. Matteres : IKEA
+3. Matteres : DUROFLEX
+4. IKEA bed iron court
+5. portable tea table 2
+6. Study Table for boy
+7. Stydy table for girl
+8. Study chair for boy
+9. Stydy chair for girl
+10. Wardrob 
+11. Wardrob 
+12. Bike : age 9-10
+13. Bike : age 4-5
+14. Samsung TV 55 inch
+15. Recliner Dufoflex single seater : count 2
+16. IKEA light 
+17. IRON Stand
+18. IRON 
+19. Chrismas tree
+20. Sofa 3 seater 
+21. IKEA Steel rack
+22. TV rack with storage
+23. IKEA TABLE : count 2
+24. Office table : 1
+25. blanket : king size 2
+26. Mixer prreti with juicer
+27. Air fryer with Oven 
+28. cashroll : milton 
+29. Superhero toys
+30. Soft toys for kids
+31. Steel Plate 
+32. Steel Glass
+33. King size bed protector 
+34. Dining chair 4
+35. Robot vaccume cleaner
+36. Humidifier
+37. Water filter
+38. Stemer
+39. Bedsheet king size 
+40. Kitchen Utencils made out of steel
+41. weight machine
+42.  
--- script.js
+++ script.js
@@ -1,77 +1,403 @@
 const items = [
   {
-    name: 'Wood Dining Table',
-    price: '$120',
-    description: 'Solid wood table with seating for 4-6 people.',
+    name: 'Air conditioner',
+    price: '$200',
+    description: 'Efficient air conditioner in good working condition.',
+    condition: 'Good',
+    images: ['img/Window Air Conditioner.avif', 'img/Window Air Conditioner-2.avif'],
+  },
+  {
+    name: 'Mattress: IKEA',
+    price: '$150',
+    description: 'Comfortable IKEA mattress.',
     condition: 'Excellent',
-    image: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4?auto=format&fit=crop&w=900&q=80',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
   },
   {
-    name: 'Cozy Sofa',
-    price: '$180',
-    description: 'Comfortable 3-seat sofa in light grey fabric.',
+    name: 'Mattress: DUROFLEX',
+    price: '$120',
+    description: 'Durable DUROFLEX mattress.',
     condition: 'Very good',
-    image: 'https://images.unsplash.com/photo-1494526585095-c41746248156?auto=format&fit=crop&w=900&q=80',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
   },
   {
-    name: 'Standing Lamp',
-    price: '$35',
-    description: 'Modern lamp with adjustable arm and soft lighting.',
+    name: 'IKEA bed iron court',
+    price: '$100',
+    description: 'IKEA bed frame with iron court.',
     condition: 'Good',
-    image: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4?auto=format&fit=crop&w=900&q=80',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
   },
   {
-    name: 'Kitchenware Set',
-    price: '$45',
-    description: 'Complete cookware set including pots, pans, and utensils.',
+    name: 'Portable tea table (2)',
+    price: '$50',
+    description: 'Two portable tea tables.',
     condition: 'Like new',
-    image: 'https://images.unsplash.com/photo-1494571924674-2a7c478b9aab?auto=format&fit=crop&w=900&q=80',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
   },
   {
-    name: 'Bookshelf',
-    price: '$60',
-    description: 'Spacious shelf for books, decor, and storage baskets.',
-    condition: 'Great',
-    image: 'https://images.unsplash.com/photo-1517511620798-cec17d428bc0?auto=format&fit=crop&w=900&q=80',
+    name: 'Study Table for boy',
+    price: '$80',
+    description: 'Study table suitable for a boy.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
   },
   {
-    name: 'Queen Mattress',
-    price: '$140',
-    description: 'Comfortable mattress in clean condition with minimal wear.',
+    name: 'Study table for girl',
+    price: '$80',
+    description: 'Study table suitable for a girl.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Study chair for boy',
+    price: '$40',
+    description: 'Study chair for a boy.',
     condition: 'Good',
-    image: 'https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?auto=format&fit=crop&w=900&q=80',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Study chair for girl',
+    price: '$40',
+    description: 'Study chair for a girl.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Wardrobe',
+    price: '$250',
+    description: 'Spacious wardrobe.',
+    condition: 'Very good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Wardrobe',
+    price: '$250',
+    description: 'Another spacious wardrobe.',
+    condition: 'Very good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Bike: age 9-10',
+    price: '$100',
+    description: 'Bike suitable for ages 9-10.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Bike: age 4-5',
+    price: '$80',
+    description: 'Bike suitable for ages 4-5.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Samsung TV 55 inch',
+    price: '$400',
+    description: '55-inch Samsung TV.',
+    condition: 'Like new',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Recliner Dufoflex single seater (count 2)',
+    price: '$150',
+    description: 'Two Dufoflex single seater recliners.',
+    condition: 'Very good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'IKEA light',
+    price: '$30',
+    description: 'IKEA lighting fixture.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'IRON Stand',
+    price: '$20',
+    description: 'Stand for iron.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'IRON',
+    price: '$25',
+    description: 'Iron for clothes.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Christmas tree',
+    price: '$50',
+    description: 'Christmas tree decoration.',
+    condition: 'Like new',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Sofa 3 seater',
+    price: '$300',
+    description: '3-seater sofa.',
+    condition: 'Very good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'IKEA Steel rack',
+    price: '$100',
+    description: 'Steel rack from IKEA.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'TV rack with storage',
+    price: '$120',
+    description: 'TV rack with storage space.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'IKEA TABLE (count 2)',
+    price: '$150',
+    description: 'Two IKEA tables.',
+    condition: 'Very good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Office table (1)',
+    price: '$100',
+    description: 'Office table.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Blanket: king size (2)',
+    price: '$40',
+    description: 'Two king-size blankets.',
+    condition: 'Like new',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Mixer Preethi with juicer',
+    price: '$80',
+    description: 'Preethi mixer with juicer.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Air fryer with Oven',
+    price: '$150',
+    description: 'Air fryer with oven function.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Cashroll: Milton',
+    price: '$10',
+    description: 'Milton cashroll.',
+    condition: 'New',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Superhero toys',
+    price: '$20',
+    description: 'Collection of superhero toys.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Soft toys for kids',
+    price: '$15',
+    description: 'Soft toys for children.',
+    condition: 'Like new',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Steel Plate',
+    price: '$5',
+    description: 'Steel plate.',
+    condition: 'New',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Steel Glass',
+    price: '$3',
+    description: 'Steel glass.',
+    condition: 'New',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'King size bed protector',
+    price: '$30',
+    description: 'King-size bed protector.',
+    condition: 'Like new',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Dining chair (4)',
+    price: '$100',
+    description: 'Four dining chairs.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Robot vacuum cleaner',
+    price: '$200',
+    description: 'Robot vacuum cleaner.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Humidifier',
+    price: '$50',
+    description: 'Humidifier for home.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Water filter',
+    price: '$40',
+    description: 'Water filter system.',
+    condition: 'Very good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Steamer',
+    price: '$25',
+    description: 'Clothes steamer.',
+    condition: 'Like new',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Bedsheet king size',
+    price: '$20',
+    description: 'King-size bedsheet.',
+    condition: 'New',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Kitchen Utensils made out of steel',
+    price: '$30',
+    description: 'Set of steel kitchen utensils.',
+    condition: 'Good',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
+  },
+  {
+    name: 'Weight machine',
+    price: '$100',
+    description: 'Weight machine for exercise.',
+    condition: 'Excellent',
+    images: ['img/moving-house-cardboard-boxes.jpg'],
   },
 ];
 
-const itemsGrid = document.getElementById('itemsGrid');
-const searchInput = document.getElementById('searchInput');
+document.addEventListener('DOMContentLoaded', () => {
+  const itemsGrid = document.getElementById('itemsGrid');
+  const searchInput = document.getElementById('searchInput');
+  const modal = document.getElementById('itemModal');
+  const modalImage = document.getElementById('modalImage');
+  const modalDetails = document.getElementById('modalDetails');
+  const closeBtn = document.querySelector('.close');
+  const prevImageBtn = document.getElementById('prevImage');
+  const nextImageBtn = document.getElementById('nextImage');
 
-function renderItems(filteredItems) {
-  itemsGrid.innerHTML = filteredItems.map(item => `
-    <article class="item-card">
-      <img src="${item.image}" alt="${item.name}" loading="lazy" />
-      <div class="item-card-content">
-        <h3 class="item-card-title">${item.name}</h3>
-        <p class="item-card-description">${item.description}</p>
-        <div class="item-card-footer">
-          <span class="item-price">${item.price}</span>
-          <span class="item-condition">${item.condition}</span>
-        </div>
+  let currentItem = null;
+  let currentImageIndex = 0;
+
+  function openModal(item) {
+    currentItem = item;
+    currentImageIndex = 0;
+    updateModalImage();
+    modalDetails.innerHTML = `
+      <h2>${item.name}</h2>
+      <p>${item.description}</p>
+      <div class="modal-footer">
+        <span class="modal-price">${item.price}</span>
+        <span class="modal-condition">${item.condition}</span>
       </div>
-    </article>
-  `).join('');
-}
-
-function filterItems(query) {
-  const normalized = query.trim().toLowerCase();
-  if (!normalized) {
-    return items;
+    `;
+    modal.style.display = 'block';
+    updateArrowVisibility();
   }
-  return items.filter(item => item.name.toLowerCase().includes(normalized) || item.description.toLowerCase().includes(normalized));
-}
 
-searchInput.addEventListener('input', () => {
-  renderItems(filterItems(searchInput.value));
+  function updateModalImage() {
+    if (currentItem && currentItem.images.length > 0) {
+      modalImage.src = currentItem.images[currentImageIndex];
+      modalImage.alt = currentItem.name;
+    }
+  }
+
+  function updateArrowVisibility() {
+    if (currentItem) {
+      prevImageBtn.style.display = currentItem.images.length > 1 ? 'flex' : 'none';
+      nextImageBtn.style.display = currentItem.images.length > 1 ? 'flex' : 'none';
+    }
+  }
+
+  function closeModal() {
+    modal.style.display = 'none';
+    currentItem = null;
+    currentImageIndex = 0;
+  }
+
+  function nextImage() {
+    if (currentItem && currentImageIndex < currentItem.images.length - 1) {
+      currentImageIndex++;
+      updateModalImage();
+    }
+  }
+
+  function prevImage() {
+    if (currentItem && currentImageIndex > 0) {
+      currentImageIndex--;
+      updateModalImage();
+    }
+  }
+
+  closeBtn.addEventListener('click', closeModal);
+
+  prevImageBtn.addEventListener('click', prevImage);
+  nextImageBtn.addEventListener('click', nextImage);
+
+  window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  function renderItems(filteredItems) {
+    itemsGrid.innerHTML = filteredItems.map((item, index) => `
+      <article class="item-card" data-index="${index}">
+        <div class="item-images">
+          ${item.images.length > 0 ? `<img src="${item.images[0]}" alt="${item.name}" loading="lazy" />` : ''}
+        </div>
+        <div class="item-card-content">
+          <h3 class="item-card-title">${item.name}</h3>
+          <p class="item-card-description">${item.description}</p>
+          <div class="item-card-footer">
+            <span class="item-price">${item.price}</span>
+            <span class="item-condition">${item.condition}</span>
+          </div>
+        </div>
+      </article>
+    `).join('');
+    // Add click event listeners
+    document.querySelectorAll('.item-card').forEach(card => {
+      card.addEventListener('click', (e) => {
+        const index = parseInt(card.dataset.index);
+        openModal(filteredItems[index]);
+      });
+    });
+  }
+
+  function filterItems(query) {
+    const normalized = query.trim().toLowerCase();
+    if (!normalized) {
+      return items;
+    }
+    return items.filter(item => item.name.toLowerCase().includes(normalized) || item.description.toLowerCase().includes(normalized));
+  }
+
+  searchInput.addEventListener('input', () => {
+    renderItems(filterItems(searchInput.value));
+  });
+
+  renderItems(items);
 });
-
-renderItems(items);
